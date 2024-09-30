c4abb58b209ce09907d02bc8844a9998e62cacde
arguetdanitzaarmijo <arguetgd79@gmail.com>
2024-09-30 03:45:54 -0400

    second part lab-07

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 
-import './App.css'
-import { WapperList } from './components/WrapperList'
-import { List } from './components/List'
-function App() {
-  return (
+import './App.css';
+import { WapperList } from './components/WrapperList';
+import { Button } from './components/Button';
+
+  function App ( ){
+    const buttonText ="count :";
+    return (
     <>
-    <WapperList>
-      <List/>
-    </WapperList> 
-
+        <h1> Contador incrementa de 5 en 5 </h1>
+        <Button text= {buttonText}/>
     </>
-  )
-}
-
-export default App
+    );
+  }
+  export default App;
